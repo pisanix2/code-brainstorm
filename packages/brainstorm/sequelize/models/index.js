b9b6a1549d1b87a5d86f26568c8537d6a23717a3
Pisani <pisanix@gmail.com>
2022-01-18 12:03:15 -0300

    ajustar timezone no postgre

--- packages/brainstorm/sequelize/models/index.js
+++ packages/brainstorm/sequelize/models/index.js
@@ -3,6 +3,11 @@ const sqlFormatter = require('sql-formatter')
 const config = require('../config')
 const logger = require('../../shared/logger')({ ctx: 'database' })
 
+require('pg').types.setTypeParser(1114, stringValue => {
+  return new Date(stringValue + '+0000');
+  // e.g., UTC offset. Use any offset that you would like.
+})
+
 const db = {}
 
 config.logging = (sql) => {
